cb30a6a3be42b99bb46a41f00ef49ac2f2e41332
gabrielmatei <matei.gabriel99@gmail.com>
2022-09-27 17:37:50 +0000

    Deploying to gh-pages from @ StakeGold/stakegold.com@4cb0335ceced966f00145e2fd40b6ef66a0e49b2 🚀

--- _next/static/W21gV9nle8G1U7-2E5gpX/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/0f999f3ed56a0a32.css","static/chunks/pages/index-03dec4e81249eb2c.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/tVk-MOHFnK3wNxXnBvFLQ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/0f999f3ed56a0a32.css","static/chunks/pages/index-fdf03820b3c12905.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
